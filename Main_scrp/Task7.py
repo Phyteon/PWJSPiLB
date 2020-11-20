0846e91d1d1d7be1a1ba711ea9600dc64cdbbe57
Pytheon <pawelecfilip6@gmail.com>
2020-11-20 20:57:05 +0100

    Completed task 7. Continuing to task 8.

--- Main_scrp/Task7.py
+++ Main_scrp/Task7.py
@@ -1,2 +1,10 @@
 
-text = open()
+from Res.task7_func import change_words
+
+
+path = r"D:\PycharmProjects\PWJSPiLB\Res\test.txt"
+
+words = ["się", "Się", "oraz", "Oraz", "nigdy", "Nigdy", "dlaczego", "Dlaczego"]
+replacements = []
+
+change_words(words, replacements, path)
---
+++ Res/task7_func.py
@@ -0,0 +1,17 @@
+import io
+
+
+def change_words(list, to_replace, file):
+    with io.open(file, 'r', encoding='utf8') as f:
+        text = f.read()
+        if len(to_replace) == 0:
+            i = 0
+            while i < len(list):
+                to_replace.append('')
+                i += 1
+
+        for (phrase, replacement) in zip(list, to_replace):
+            text = text.replace(phrase, replacement)
+
+    with io.open(file, 'w', encoding='utf8') as f_prim:
+        f_prim.write(text)
