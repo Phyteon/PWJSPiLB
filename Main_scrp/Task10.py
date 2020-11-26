3d8e9b9c95d84434f3616f5fc00ad6aa444e7e71
Pytheon <pawelecfilip6@gmail.com>
2020-11-26 21:02:39 +0100

    Testing func for task 10. Still needs debug!!!

--- Main_scrp/Task10.py
+++ Main_scrp/Task10.py
@@ -1,2 +1,10 @@
 
+import random
+from Res.task10_func import radix_lsd_sort
 # LSD radix sorting algorithm
+
+randints = [101, 63, 50, 13, 10, 5, 3, 1]
+# for i in range(0, 50):
+#     randints.append(random.randint(0, 200))
+
+srtd = radix_lsd_sort(randints)
--- Res/task10_func.py
+++ Res/task10_func.py
@@ -12,35 +12,62 @@ def radix_lsd_sort(data):
         for element in sym_data:
             if element[-1] == str(lsd):
                 aux.append(element)
-        buckets.append(aux)
+        if len(aux) > 0:
+            buckets.append(aux)
 
     n_of_digit = -2  # Indexing from the end of the string (LSD)
     while True:
         control = 0
         aux_buckets = []
         for nd in range(0, 10):
+            wtf = buckets[:]
             aux = []
             for nested in buckets:
-                for element in nested:
-                    if -n_of_digit > len(element):
-                        control += 1
+                if len(nested) > 0:
+                    for element in nested:
+                        global_idx = buckets.index(nested)
                         local_idx = nested.index(element)
-                        if len(aux_buckets) == 0:
-                            aux_buckets.append([nested.pop(local_idx)])
-                        elif len(aux_buckets[0][0]) < -n_of_digit: # TODO: Debug this section (string index out of range?)
-                            aux_buckets[0].append(nested.pop(local_idx))
-                        elif aux_buckets[0][0][n_of_digit] == '0':
-                            aux_buckets[0].append(nested.pop(local_idx))
-                        else:
-                            aux_buckets.insert(0, [nested.pop(local_idx)])
-                    elif str(nd) == element[n_of_digit]:
-                        local_idx = nested.index(element)
-                        aux.append(nested.pop(local_idx))
-            aux_buckets.append(aux)
+                        if -n_of_digit > len(element):
+                            control += 1
+                            if len(aux_buckets) == 0:
+                                aux_buckets.append([element])
+                                wtf[global_idx].pop(local_idx)
+                            elif len(aux_buckets[0][0]) < -n_of_digit:
+                                aux_buckets[0].append(element)
+                                wtf[global_idx].pop(local_idx)
+                            elif aux_buckets[0][0][n_of_digit] == '0':
+                                aux_buckets[0].append(element)
+                                wtf[global_idx].pop(local_idx)
+                            else:
+                                aux_buckets.insert(0, [element])
+                                wtf[global_idx].pop(local_idx)
+                        elif str(nd) == element[n_of_digit]:
+                            if nd == 0:
+                                if len(aux_buckets) != 0:
+                                    if len(aux_buckets[0][0]) < -n_of_digit:
+                                        aux_buckets[0].append(element)
+                                        wtf[global_idx].pop(local_idx)
+                                    elif aux_buckets[0][0][n_of_digit] == '0':
+                                        aux_buckets[0].append(element)
+                                        wtf[global_idx].pop(local_idx)
+                                    else:
+                                        aux.append(element)
+                                        wtf[global_idx].pop(local_idx)
+                                else:
+                                    aux_buckets.append([element])
+                                    wtf[global_idx].pop(local_idx)
+                            else:
+                                aux.append(element)
+                                wtf[global_idx].pop(local_idx)
+
+            if len(aux) > 0:
+                aux_buckets.append(aux)
+            buckets = wtf
         buckets = aux_buckets
         if control == volume:
             break
         else:
+            control = 0
             n_of_digit -= 1
     srtd = []
     for elem_list in buckets:
