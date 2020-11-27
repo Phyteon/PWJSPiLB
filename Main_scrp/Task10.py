6330f9e2c5bacc429c36e8c1de43210aac72dd6d
Pytheon <pawelecfilip6@gmail.com>
2020-11-27 01:04:42 +0100

    Tested and proved.

--- Main_scrp/Task10.py
+++ Main_scrp/Task10.py
@@ -1,12 +1,16 @@
 
+import time
 import random
+import copy
 from Res.task10_func import radix_lsd_sort
 # LSD radix sorting algorithm
-
+time_start = time.time()
 randints = []
-for i in range(0, 20):
+for i in range(0, 100000):
     randints.append(random.randint(0, 999))
 
-print(randints)
+good = copy.deepcopy(randints)
+good.sort()
 srtd = radix_lsd_sort(randints)
-print(srtd)
+if good == srtd:
+    print("Git")
